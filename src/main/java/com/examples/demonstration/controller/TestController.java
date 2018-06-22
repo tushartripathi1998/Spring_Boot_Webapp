5a80b7c68dcd7c6e308f596d2e8afb097da5e60d
tushartripathi1998 <thetushartripathi1998@gmail.com>
2018-06-22 09:11:37 +0530

    First Commit

---
+++ src/main/java/com/examples/demonstration/controller/TestController.java
@@ -0,0 +1,5 @@
+package com.examples.demonstration.controller;
+
+public class TestController {
+
+}
---
+++ src/test/java/com/examples/demonstration/DemonstrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.examples.demonstration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemonstrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
